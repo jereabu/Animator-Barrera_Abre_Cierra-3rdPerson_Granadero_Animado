94532acb345da6090a35d04776a0cd279891ecf2
Jeremias_Abulafia <jeremiasabulafia@gmail.com>
2022-10-04 10:31:59 -0300

    done excercises

---
+++ Assets/Scripts/agentscript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class agentscript : MonoBehaviour
+{
+    NavMeshAgent agent;
+    [SerializeField] Transform targetTransform;
+    [SerializeField] Transform Spawn;
+    [SerializeField] Transform previousDestination;
+    [SerializeField] Transform[] destinations;
+    [SerializeField] float arrivingDistance;
+    int counter;
+    bool Chase = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Chase == true)
+        {
+            agent.destination = targetTransform.position;
+        }
+        else
+        {
+            if(agent.remainingDistance < arrivingDistance && !agent.pathPending)
+            {
+                counter++;
+                if(counter >= destinations.Length)
+                {
+                    counter = 0;
+                }
+                agent.destination = destinations[counter].position;
+            }
+            
+        }
+     
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            Chase = true;
+        }
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            Chase = false;
+        }
+    }
+}
